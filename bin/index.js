0fb401e26d6a70a402867e7c0eda69a3fd0b41cc
Tim Paulaskas <drookue@gmail.com>
2020-06-05 16:57:24 -0700

    Added env for local testing

--- bin/index.js
+++ bin/index.js
@@ -123,14 +123,23 @@ const packageArray = {
     WorkflowTask: "workflows"
 }
 
+const demandOption = process.env.YARG_DEMAND === 'true' || process.env.YARG_DEMAND === undefined
+
 const options = yargs
-    .usage("Usage: tic-sfpkg -s <force-app-folder> -d <vscode-project> -f <package.xml> -u <slack-webhook-url>")
-    .option("s", { alias: "src", describe: "Source directory", type: "string", demandOption: true })
-    .option("d", { alias: "deploy", describe: "Deploy directory", type: "string", demandOption: true })
-    .option("f", { alias: "pkgxml", describe: "package.xml", type: "string", demandOption: true })
-    .option("u", { alias: "slackurl", describe: "Slack URL", type: "string", demandOption: false})
+    .usage('Usage: tic-sfpkg -s <force-app-folder> -d <vscode-project> -f <package.xml> -u <slack-webhook-url>')
+    .option('s', { alias: 'src', describe: 'Source directory', type: 'string', demandOption })
+    .option('d', { alias: 'deploy', describe: 'Deploy directory', type: 'string', demandOption: false })
+    .option('f', { alias: 'pkgxml', describe: 'package.xml', type: 'string', demandOption: false })
+    .option('u', { alias: 'slackurl', describe: 'Slack URL', type: 'string', demandOption: false})
     .argv;
 
+
+if (process.env.YARG_DEMAND) {
+    options.slackurl = process.env.SLACK_WEBHOOK_URL
+    options.src = process.env.SRC_PATH
+    options.deploy = process.env.DEST_PATH
+    options.pkgxml = process.env.XML_PATH
+}
 
 const Directories = [];
 
