612bb9666695fad7b3d7b708906b35b611869235
Tim Paulaskas <drookue@gmail.com>
2020-06-05 17:06:10 -0700

    Refactored slack notifications

--- bin/index.js
+++ bin/index.js
@@ -5,8 +5,10 @@ const boxen = require("boxen");
 const yargs = require("yargs");
 const xml2js = require('xml2js');
 const fs = require('fs');
-const https = require('https');
 const colors = require('colors');
+
+const notifySlack = require('../src/notifications/slack')
+
 const stringProcessing = colors.green('     Processing metadata category:');
 const boxenOptions = {
     padding: 1,
@@ -22,7 +24,7 @@ const userAccountNotification = {
 };
 
 const packageArray = {
-    ApexClass: "classes",
+    //ApexClass: "classes",
     ApexComponent: "components",
     ApexPage: "pages",
     ApexTestSuite: "testSuites",
@@ -143,69 +145,6 @@ if (process.env.YARG_DEMAND) {
 
 const Directories = [];
 
-/**
-* Handles the actual sending request.
-* We're turning the https.request into a promise here for convenience
-* @param webhookURL
-* @param messageBody
-* @return {Promise}
-*/
-function sendSlackMessage(webhookURL, messageBody) {
-    // make sure the incoming message body can be parsed into valid JSON
-    try {
-        messageBody = JSON.stringify(messageBody);
-    } catch (e) {
-        console.log('Failed to stringify messageBody for Slack');
-        throw new Error('Failed to stringify messageBody for Slack', e);
-    }
-
-    // Promisify the https.request
-    return new Promise((resolve, reject) => {
-        // general request options, we defined that it's a POST request and content is JSON
-        const requestOptions = {
-            method: 'POST',
-            header: {
-                'Content-Type': 'application/json'
-            }
-        };
-
-        // actual request
-        const req = https.request(webhookURL, requestOptions, (res) => {
-            let response = '';
-
-
-            res.on('data', (d) => {
-                response += d;
-            });
-
-            // response finished, resolve the promise with data
-            res.on('end', () => {
-                resolve(response);
-            })
-        });
-
-        // there was an error, reject the promise
-        req.on('error', (e) => {
-            reject(e);
-        });
-
-        // send our message body (was parsed to JSON beforehand)
-        req.write(messageBody);
-        req.end();
-    });
-}
-
-async function notifySlack(message) {
-    url = `${options.slackurl}`;
-    if (url !== 'undefined' && url !== null && url != '') {
-        try {
-            const slackResponse = await sendSlackMessage(url, message);
-            //console.log('Message response', slackResponse);
-        } catch (e) {
-            console.error('\nThere was a error with the slack request. Continuing execution.\n', e);
-        }
-    }
-}
 
 function exitApp(message) {
     const boxenOptions = {
@@ -332,7 +271,7 @@ function checkNodes(obj){
     }
 
     if (userAccountNotification.attachments.length > 0) {
-        notifySlack(userAccountNotification);
+        notifySlack(userAccountNotification, options.slackurl);
     }
 }
 
---
+++ src/notifications/slack.js
@@ -0,0 +1,66 @@
+const https = require('https');
+
+/**
+* Handles the actual sending request.
+* We're turning the https.request into a promise here for convenience
+* @param webhookURL
+* @param messageBody
+* @return {Promise}
+*/
+function sendSlackMessage(webhookURL, messageBody) {
+    // make sure the incoming message body can be parsed into valid JSON
+    try {
+        messageBody = JSON.stringify(messageBody);
+    } catch (e) {
+        console.log('Failed to stringify messageBody for Slack');
+        throw new Error('Failed to stringify messageBody for Slack', e);
+    }
+
+    // Promisify the https.request
+    return new Promise((resolve, reject) => {
+        // general request options, we defined that it's a POST request and content is JSON
+        const requestOptions = {
+            method: 'POST',
+            header: {
+                'Content-Type': 'application/json'
+            }
+        };
+
+        // actual request
+        const req = https.request(webhookURL, requestOptions, (res) => {
+            let response = '';
+
+
+            res.on('data', (d) => {
+                response += d;
+            });
+
+            // response finished, resolve the promise with data
+            res.on('end', () => {
+                resolve(response);
+            })
+        });
+
+        // there was an error, reject the promise
+        req.on('error', (e) => {
+            reject(e);
+        });
+
+        // send our message body (was parsed to JSON beforehand)
+        req.write(messageBody);
+        req.end();
+    });
+}
+
+async function notifySlack(message, url) {
+    if (url !== 'undefined' && url !== null && url != '') {
+        try {
+            const slackResponse = await sendSlackMessage(url, message);
+            //console.log('Message response', slackResponse);
+        } catch (e) {
+            console.error('\nThere was a error with the slack request. Continuing execution.\n', e);
+        }
+    }
+}
+
+module.exports = notifySlack
